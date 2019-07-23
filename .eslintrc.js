de1c8c8d2dbc13a17fbc3a0b49f11180e131d413
smhg <sam.hauglustaine@gmail.com>
2019-07-23 10:40:34 +0200

    run eslint init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,21 @@
 module.exports = {
-    "extends": "standard",
-    "rules": {
-        "semi": [2, "always"]
-    }
-};
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    'semi': [2, 'always']
+  }
+}
